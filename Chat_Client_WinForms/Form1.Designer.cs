81a01f987b7e796820883592f78c37abdecb8dcf
Doug Wagner <digwagner@gmail.com>
2016-06-01 17:54:53 -0700

    initial commit

---
+++ Chat_Client_WinForms/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace Chat_Client_WinForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ChatText = new System.Windows.Forms.TextBox();
+            this.InputText = new System.Windows.Forms.TextBox();
+            this.SendButton = new System.Windows.Forms.Button();
+            this.IPInput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ConnectButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.UsernameInput = new System.Windows.Forms.TextBox();
+            this.Users = new System.Windows.Forms.ListBox();
+            this.SendToAll = new System.Windows.Forms.RadioButton();
+            this.SendToOne = new System.Windows.Forms.RadioButton();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // ChatText
+            // 
+            this.ChatText.Cursor = System.Windows.Forms.Cursors.Default;
+            this.ChatText.Location = new System.Drawing.Point(300, 107);
+            this.ChatText.Multiline = true;
+            this.ChatText.Name = "ChatText";
+            this.ChatText.ReadOnly = true;
+            this.ChatText.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.ChatText.Size = new System.Drawing.Size(1226, 465);
+            this.ChatText.TabIndex = 0;
+            // 
+            // InputText
+            // 
+            this.InputText.Location = new System.Drawing.Point(300, 578);
+            this.InputText.Multiline = true;
+            this.InputText.Name = "InputText";
+            this.InputText.Size = new System.Drawing.Size(977, 208);
+            this.InputText.TabIndex = 1;
+            // 
+            // SendButton
+            // 
+            this.SendButton.Enabled = false;
+            this.SendButton.Location = new System.Drawing.Point(1283, 578);
+            this.SendButton.Name = "SendButton";
+            this.SendButton.Size = new System.Drawing.Size(243, 208);
+            this.SendButton.TabIndex = 2;
+            this.SendButton.Text = "Send";
+            this.SendButton.UseVisualStyleBackColor = true;
+            this.SendButton.Click += new System.EventHandler(this.SendButton_Click);
+            // 
+            // IPInput
+            // 
+            this.IPInput.Location = new System.Drawing.Point(428, 41);
+            this.IPInput.Name = "IPInput";
+            this.IPInput.Size = new System.Drawing.Size(270, 31);
+            this.IPInput.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(300, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 25);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "IP Address:";
+            // 
+            // ConnectButton
+            // 
+            this.ConnectButton.Location = new System.Drawing.Point(1367, 12);
+            this.ConnectButton.Name = "ConnectButton";
+            this.ConnectButton.Size = new System.Drawing.Size(159, 89);
+            this.ConnectButton.TabIndex = 5;
+            this.ConnectButton.Text = "Connect";
+            this.ConnectButton.UseVisualStyleBackColor = true;
+            this.ConnectButton.Click += new System.EventHandler(this.ConnectButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(704, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(116, 25);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Username:";
+            // 
+            // UsernameInput
+            // 
+            this.UsernameInput.Location = new System.Drawing.Point(826, 41);
+            this.UsernameInput.MaxLength = 32;
+            this.UsernameInput.Name = "UsernameInput";
+            this.UsernameInput.Size = new System.Drawing.Size(535, 31);
+            this.UsernameInput.TabIndex = 7;
+            // 
+            // Users
+            // 
+            this.Users.FormattingEnabled = true;
+            this.Users.ItemHeight = 25;
+            this.Users.Location = new System.Drawing.Point(12, 107);
+            this.Users.Name = "Users";
+            this.Users.Size = new System.Drawing.Size(282, 679);
+            this.Users.TabIndex = 8;
+            // 
+            // SendToAll
+            // 
+            this.SendToAll.AutoSize = true;
+            this.SendToAll.Checked = true;
+            this.SendToAll.Location = new System.Drawing.Point(12, 12);
+            this.SendToAll.Name = "SendToAll";
+            this.SendToAll.Size = new System.Drawing.Size(154, 29);
+            this.SendToAll.TabIndex = 9;
+            this.SendToAll.TabStop = true;
+            this.SendToAll.Text = "Send To All";
+            this.SendToAll.UseVisualStyleBackColor = true;
+            // 
+            // SendToOne
+            // 
+            this.SendToOne.AutoSize = true;
+            this.SendToOne.Location = new System.Drawing.Point(12, 47);
+            this.SendToOne.Name = "SendToOne";
+            this.SendToOne.Size = new System.Drawing.Size(170, 29);
+            this.SendToOne.TabIndex = 10;
+            this.SendToOne.TabStop = true;
+            this.SendToOne.Text = "Send To One";
+            this.SendToOne.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(78, 79);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(136, 25);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Users Online";
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.SendButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1538, 797);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.SendToOne);
+            this.Controls.Add(this.SendToAll);
+            this.Controls.Add(this.Users);
+            this.Controls.Add(this.UsernameInput);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.ConnectButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.IPInput);
+            this.Controls.Add(this.SendButton);
+            this.Controls.Add(this.InputText);
+            this.Controls.Add(this.ChatText);
+            this.Name = "Form1";
+            this.Text = "BDDBMSA Chat Client";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox ChatText;
+        private System.Windows.Forms.TextBox InputText;
+        private System.Windows.Forms.Button SendButton;
+        private System.Windows.Forms.TextBox IPInput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button ConnectButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox UsernameInput;
+        private System.Windows.Forms.ListBox Users;
+        private System.Windows.Forms.RadioButton SendToAll;
+        private System.Windows.Forms.RadioButton SendToOne;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Chat_Client_WinForms/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Chat_Client_WinForms
+{
+    public partial class Form1 : Form
+    {
+        private TcpClient client = null;
+        private NetworkStream stream = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void ConnectButton_Click(object sender, EventArgs e)
+        {
+            if (UsernameInput.Text == "") // Reject blank usernames.
+            {
+                ChatText.AppendText("Username input is blank.\r\n");
+                return;
+            }
+            try
+            {
+                IPAddress address = IPAddress.Parse(IPInput.Text);
+                client = new TcpClient();
+                await client.ConnectAsync(address, 42424); // Connect to server at specified address.
+                stream = client.GetStream(); // Obtain network stream.
+                await SendMessage(UsernameInput.Text, 1); // Send initial connection message.
+            }
+            catch (Exception ex)
+            {
+                ChatText.AppendText(ex.Message + "\r\n");
+                return;
+            }
+            /* Change form controls after connection established. */
+            IPInput.ReadOnly = true;
+            IPInput.Cursor = Cursors.Default;
+            UsernameInput.ReadOnly = true;
+            UsernameInput.Cursor = Cursors.Default;
+            ConnectButton.Enabled = false;
+            SendButton.Enabled = true;
+            while (true)
+            {
+                try {
+                    await WaitForMessages(); // Loop to recieve messages from server.
+                }
+                catch (Exception ex)
+                {
+                    /* Revert form controls if connection to server is lost. */
+                    ChatText.AppendText(ex.Message + "\r\n");
+                    IPInput.ReadOnly = false;
+                    IPInput.Cursor = Cursors.IBeam;
+                    UsernameInput.ReadOnly = false;
+                    UsernameInput.Cursor = Cursors.IBeam;
+                    ConnectButton.Enabled = true;
+                    SendButton.Enabled = false;
+                    Users.Items.Clear();
+                    break;
+                }
+            }
+        }
+
+        private async void SendButton_Click(object sender, EventArgs e)
+        {
+            if (SendToAll.Checked)
+                await SendMessage(InputText.Text, 2); // Send message to server.
+            else if (SendToOne.Checked)
+            {
+                if (Users.Text == "")
+                {
+                    ChatText.AppendText("No currently selected user.");
+                    return;
+                }
+                await SendMessage(InputText.Text, 3); // Send message to server.
+                await SendMessage(Users.Text, 0); // Send recieving username to server.
+            }
+            InputText.Text = "";
+        }
+
+        private async Task SendMessage(string message, byte flag)
+        {
+            byte[] stringBytes = Encoding.ASCII.GetBytes(message);
+            byte[] sendMessage;
+            byte[] messageSize = BitConverter.GetBytes(stringBytes.Length);
+            if (flag == 0)
+            {
+                /* If flag is 0, sendMessage indices 0-3 are reserved for size of string being sent. */
+                sendMessage = new byte[stringBytes.Length + 4];
+                messageSize.CopyTo(sendMessage, 0);
+                stringBytes.CopyTo(sendMessage, 4);
+            }
+            else
+            {
+                /* If flag is not 0, sendMessage index 0 is reserved for message type flag, and indices
+                   1-4 are reserved for the size of the string being sent. */
+                sendMessage = new byte[stringBytes.Length + 5];
+                sendMessage[0] = flag;
+                messageSize.CopyTo(sendMessage, 1);
+                stringBytes.CopyTo(sendMessage, 5);
+            }
+            await stream.WriteAsync(sendMessage, 0, sendMessage.Length);
+        }
+
+        private async Task WaitForMessages()
+        {
+            byte[] messageFlag = new byte[1];
+            int x = await stream.ReadAsync(messageFlag, 0, messageFlag.Length);
+            switch (messageFlag[0])
+            {
+                case 1: // Standard message for ChatText box.
+                    await ProcessStandardMessage();
+                    break;
+                case 2: // Username message for Users list.
+                    await ProcessUsernameMessage();
+                    break;
+                case 3: // Username disconnect message for Users list.
+                    await ProcessUserDisconnectMessage();
+                    break;
+                default: // Invalid flag.
+                    ChatText.AppendText("Invalid Message Flag\r\n");
+                    break;
+            }
+        }
+
+        private async Task ProcessStandardMessage()
+        {
+            string message = await ReadMessageBytes();
+            message = message + "\r\n";
+            ChatText.AppendText(message); // Append message to chat box.
+        }
+
+        private async Task ProcessUsernameMessage()
+        {
+            string message = await ReadMessageBytes();
+            Users.Items.Add(message); // Add user to list of users.
+        }
+
+        private async Task ProcessUserDisconnectMessage()
+        {
+            string message = await ReadMessageBytes();
+            Users.Items.Remove(message); // Remove user from list of users.
+        }
+
+        private async Task<string> ReadMessageBytes()
+        {
+            int x, size;
+            byte[] sizeBytes = new byte[4];
+            x = await stream.ReadAsync(sizeBytes, 0, sizeBytes.Length);
+            size = BitConverter.ToInt32(sizeBytes, 0);
+            byte[] buffer = new byte[size];
+            string message = null;
+            x = await stream.ReadAsync(buffer, 0, buffer.Length);
+            message += Encoding.ASCII.GetString(buffer);
+            return message;
+        }
+    }
+}
